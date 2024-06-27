d7bfde24d9aa2123844cdd8654a1805170c08ec2
GI-gabriel <161953323+GI-gabriel@users.noreply.github.com>
2024-06-27 14:53:58 -0300

    Agrega HTML, JS y Py para altas

--- back/app/app.py
+++ back/app/app.py
@@ -1,7 +1,7 @@
 #--------------------------------------------------------------------
 # Instalar con pip install Flask
 from flask import Flask, request, jsonify, render_template
-#from flask import request
+# from flask import request
 
 # Instalar con pip install flask-cors
 from flask_cors import CORS
@@ -19,6 +19,10 @@ import time
 
 app = Flask(__name__)
 CORS(app) # Esto habilitará CORS para todas las rutas
+
+# Carpeta para guardar las imagenes.
+RUTA_DESTINO = './static/img/'
+# RUTA_DESTINO = '/home/GIgabriel/mysite/static/imagenes'
 
 #--------------------------------------------------------------------
 class Catalogo:
@@ -78,7 +82,8 @@ class Catalogo:
 
         # Se completa la tabla de brokers de forma estática,
         # no se va a utilizar un CRUD para esta tabla
-        url_img_path = "./static/img/brokers/"
+        # url_img_path = "./static/img/brokers/"
+        url_img_path = RUTA_DESTINO + 'brokers/'
         brokers = [
             {'name': 'John Doe', 'phone': '123-456-7890', 'email': 'john.doe@example.com', 'url_img': url_img_path + 'm1' + '.jpg'},
             {'name': 'Jane Smith', 'phone': '987-654-3210', 'email': 'jane.smith@example.com', 'url_img': url_img_path + 'f1' + '.jpg'},
@@ -93,7 +98,8 @@ class Catalogo:
         ]
 
         self.cursor.execute("SELECT COUNT(*) FROM brokers")
-        # Si el resultado es mayor a 0, la tabla existe        
+        # Si el resultado es mayor a 0, la tabla existe
+        # Entonces no es necesario llenarla nuevamente       
         if not (self.cursor.fetchone()[0]) > 0:
             for broker in brokers:
                 self.cursor.execute(f'''INSERT INTO brokers (nombre, mail, telefono, url_foto)
@@ -110,73 +116,98 @@ class Catalogo:
 
 
     #----------------------------------------------------------------
-    # def agregar_producto(self,
-    #                       descripcion,
-    #                         cantidad,
-    #                           precio,
-    #                             imagen,
-    #                               proveedor):
-    #     sql = '''INSERT INTO propiedades (
-    #                 descripcion,
+    def agregar_prop(self,
+                     descrip_corta, descrip_larga, direccion,
+                     nota, url_foto_1, url_foto_2, url_foto_3,
+                     url_maps, id_broker, precio, superf,
+                     superf_tot, baños, dormitorios, cocheras,
+                     basicos, servicios, amenities):
+        sql = '''INSERT INTO propiedades (
+                    descrip_corta, descrip_larga, direccion,
+                    nota, url_foto_1, url_foto_2, url_foto_3,
+                    url_maps, id_broker, precio, superf,
+                    superf_tot, baños, dormitorios, cocheras,
+                    basicos, servicios, amenities )
+                 VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                          %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
 
-    #                   cantidad,
-    #                     precio,
-    #                       imagen_url,
-    #                         proveedor
-    #                         ) VALUES (%s, %s, %s, %s, %s)'''
-    #     valores = (descripcion, cantidad, precio, imagen, proveedor)
+        valores = (descrip_corta, descrip_larga, direccion,
+                   nota, url_foto_1, url_foto_2, url_foto_3,
+                   url_maps, id_broker, precio, superf,
+                   superf_tot, baños, dormitorios, cocheras,
+                   basicos, servicios, amenities)
         
-    #     self.cursor.execute(sql, valores)
-    #     self.conn.commit()
-    #     return self.cursor.lastrowid
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.lastrowid
     
     
     #----------------------------------------------------------------
-    def consultar_producto(self, codigo):
-        # Consultamos un producto a partir de su código
-        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
+    def consultar_prop(self, id):
+        # Consultamos a partir de su código
+        self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {id}")
         return self.cursor.fetchone()
     
 
     #----------------------------------------------------------------
-    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
-        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, imagen_url = %s, proveedor = %s WHERE codigo = %s"
-        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor, codigo)
+    def modificar_prop(self, id,
+                       descrip_corta, descrip_larga, direccion,
+                       nota, url_foto_1, url_foto_2, url_foto_3,
+                       url_maps, id_broker, precio, superf,
+                       superf_tot, baños, dormitorios, cocheras,
+                       basicos, servicios, amenities):
+        
+        # CUIDADO! - Tienen el mismo nombre, pero no son los argumentos de la función
+        # Son los parámetros de la consulta
+        sql = '''UPDATE productos SET 
+                    descrip_corta = %s, descrip_larga = %s, direccion = %s,
+                    nota = %s, url_foto_1 = %s, url_foto_2 = %s, url_foto_3 = %s,
+                    url_maps = %s, id_broker = %s, precio = %s, superf = %s,
+                    superf_tot = %s, baños = %s, dormitorios = %s, cocheras = %s,
+                    basicos = %s, servicios = %s, amenities = %s
+                WHERE id = %s'''
+        
+        valores = (descrip_corta, descrip_larga, direccion,
+                   nota, url_foto_1, url_foto_2, url_foto_3,
+                   url_maps, id_broker, precio, superf,
+                   superf_tot, baños, dormitorios, cocheras,
+                   basicos, servicios, amenities, id)
+
         self.cursor.execute(sql, valores)
         self.conn.commit()
         return self.cursor.rowcount > 0
 
 
     #----------------------------------------------------------------
-    def listar_productos(self):
-        self.cursor.execute("SELECT * FROM productos")
-        productos = self.cursor.fetchall()
-        return productos
+    def listar_prop(self):
+        self.cursor.execute("SELECT * FROM propiedades")
+        propiedades = self.cursor.fetchall()
+        return propiedades
     
 
     #----------------------------------------------------------------
-    def eliminar_producto(self, codigo):
-        # Eliminamos un producto de la tabla a partir de su código
-        self.cursor.execute(f"DELETE FROM productos WHERE codigo = {codigo}")
+    def eliminar_prop(self, id):
+        # Eliminamos de la tabla a partir de su código
+        self.cursor.execute(f"DELETE FROM productos WHERE codigo = {id}")
         self.conn.commit()
         return self.cursor.rowcount > 0
 
 
     #----------------------------------------------------------------
-    def mostrar_producto(self, codigo):
-        # Mostramos los datos de un producto a partir de su código
-        producto = self.consultar_producto(codigo)
-        if producto:
-            print("-" * 40)
-            print(f"Código.....: {producto['codigo']}")
-            print(f"Descripción: {producto['descripcion']}")
-            print(f"Cantidad...: {producto['cantidad']}")
-            print(f"Precio.....: {producto['precio']}")
-            print(f"Imagen.....: {producto['imagen_url']}")
-            print(f"Proveedor..: {producto['proveedor']}")
-            print("-" * 40)
-        else:
-            print("Producto no encontrado.")
+    # def mostrar_producto(self, id):
+    #     # Mostramos los datos a partir de su código
+    #     propiedad = self.consultar_producto(id)
+    #     if propiedad:
+    #         print("-" * 40)
+    #         print(f"Código.....: {producto['codigo']}")
+    #         print(f"Descripción: {producto['descripcion']}")
+    #         print(f"Cantidad...: {producto['cantidad']}")
+    #         print(f"Precio.....: {producto['precio']}")
+    #         print(f"Imagen.....: {producto['imagen_url']}")
+    #         print(f"Proveedor..: {producto['proveedor']}")
+    #         print("-" * 40)
+    #     else:
+    #         print("Producto no encontrado.")
 
 
 #--------------------------------------------------------------------
@@ -190,74 +221,106 @@ catalogo = Catalogo(host='localhost', user='root', password='', database='miapp'
 #                     database='m10808_miapp')
 
 
-# Carpeta para guardar las imagenes.
-RUTA_DESTINO = '../static/img/'
-# RUTA_DESTINO = '/home/GIgabriel/mysite/static/imagenes'
+#--------------------------------------------------------------------
+# Listar todos
+#--------------------------------------------------------------------
+# El método devuelve una lista con todas las propiedades en formato JSON.
+@app.route("/propiedades", methods=["GET"])
+def listar_prop():
+    propiedades = catalogo.listar_prop()
+    return jsonify(propiedades)
 
 
 #--------------------------------------------------------------------
-# Listar todos los productos
+# Mostrar uno sólo, según su id
 #--------------------------------------------------------------------
-#La ruta Flask /productos con el método HTTP GET está diseñada para proporcionar los detalles de todos los productos almacenados en la base de datos.
-#El método devuelve una lista con todos los productos en formato JSON.
-@app.route("/productos", methods=["GET"])
-def listar_productos():
-    productos = catalogo.listar_productos()
-    return jsonify(productos)
-
-
-#--------------------------------------------------------------------
-# Mostrar un sólo producto según su código
-#--------------------------------------------------------------------
-#La ruta Flask /productos/<int:codigo> con el método HTTP GET está diseñada para proporcionar los detalles de un producto específico basado en su código.
-#El método busca en la base de datos el producto con el código especificado y devuelve un JSON con los detalles del producto si lo encuentra, o None si no lo encuentra.
-@app.route("/productos/<int:codigo>", methods=["GET"])
-def mostrar_producto(codigo):
-    producto = catalogo.consultar_producto(codigo)
-    if producto:
-        return jsonify(producto), 201
+# El método busca en la base de datos la propiedad con el id especificado y 
+# devuelve un JSON con los detalles si lo encuentra, o None si no lo encuentra.
+@app.route("/propiedades/<int:codigo>", methods=["GET"])
+def mostrar_prop(id):
+    propiedad = catalogo.consultar_prop(id)
+    if propiedad:
+        return jsonify(propiedad), 201
     else:
-        return "Producto no encontrado", 404
+        return "Propiedad no encontrada", 404
     
 
 #--------------------------------------------------------------------
-# Agregar un producto
+# Agregar uno
 #--------------------------------------------------------------------
-@app.route("/productos", methods=["POST"])
-#La ruta Flask `/productos` con el método HTTP POST está diseñada para permitir la adición de un nuevo producto a la base de datos.
-#La función agregar_producto se asocia con esta URL y es llamada cuando se hace una solicitud POST a /productos.
-def agregar_producto():
-    #Recojo los datos del form
-    descripcion = request.form['descripcion']
-    cantidad = request.form['cantidad']
+@app.route("/propiedades", methods=["POST"])
+# La función agregar_prop se asocia con esta URL y es llamada cuando se hace
+# una solicitud POST a /propiedades.
+def agregar_prop():
+    descrip_corta = request.form['descrip_corta']
+    descrip_larga = request.form['descrip_larga']
+    direccion = request.form['direccion']
+    nota = request.form['nota']
+    foto_1 = request.files['url_foto_1']
+    foto_2 = request.files['url_foto_2']
+    foto_3 = request.files['url_foto_3']
+    url_maps = request.form['url_maps']
+    id_broker = request.form['id_broker']
     precio = request.form['precio']
-    imagen = request.files['imagen']
-    proveedor = request.form['proveedor']
-    nombre_imagen=""
+    superf = request.form['superf']
+    superf_tot = request.form['superf_tot']
+    baños = request.form['baños']
+    dormitorios = request.form['dormitorios']
+    cocheras = request.form['cocheras']
+    basicos = request.form['basicos']
+    servicios = request.form['servicios']
+    amenities = request.form['amenities']
+    
+    ########################################################################
+    # Genera nombres de imagenes con un timestamp para evitar conflictos si
+    # se cargan imagenes con el mismo nombre base
+    imgs = [foto_1, foto_2, foto_3]
+    img_urls = []
 
+    i = 0
+    for img in imgs:
+        # Chequea el nombre del archivo de la imagen, asegurándose de que sea seguro
+        # para guardar en el sistema de archivos
+        img_name = secure_filename(img.filename)
+        # Separa el nombre del archivo de su extensión
+        nombre_base, extension = os.path.splitext(img_name)     
+        # Nombre guión bajo timestamp
+        img_urls.append(f"{nombre_base}_{int(time.time())}_{i}{extension}")
+        i = i + 1
+    #
+    # 
+    ########################################################################
+    
+    #Agrega propiedad a una fila de la tabla
+    nuevo_id = catalogo.agregar_prop(descrip_corta, descrip_larga, direccion,
+                                     nota, img_urls[0], img_urls[1], img_urls[2],
+                                     url_maps, id_broker, precio, superf,
+                                     superf_tot, baños, dormitorios, cocheras,
+                                     basicos, servicios, amenities)
+    # Si el guardado en base de datos fue exitoso, guarda los archivos de imagenes
+    # en el sistema de archivos    
+    if nuevo_id:
+        # Guarda archivos de imagenes
+        i = 0
+        for img in imgs:
+            img.save(os.path.join(RUTA_DESTINO, img_urls[i]))
+            i = i + 1
 
-    # Genero el nombre de la imagen
-    nombre_imagen = secure_filename(imagen.filename) #Chequea el nombre del archivo de la imagen, asegurándose de que sea seguro para guardar en el sistema de archivos
-    nombre_base, extension = os.path.splitext(nombre_imagen) #Separa el nombre del archivo de su extensión.
-    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
-    #Genera un nuevo nombre para la imagen usando un timestamp, para evitar sobreescrituras y conflictos de nombres.
-
-    nuevo_codigo = catalogo.agregar_producto(descripcion, cantidad, precio, nombre_imagen, proveedor)
-    if nuevo_codigo:
-        imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
-        #Si el producto se agrega con éxito, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 201 (Creado).
-        return jsonify({"mensaje": "Producto agregado correctamente.", "codigo": nuevo_codigo, "imagen": nombre_imagen}), 201
+        # Si el producto se agrega con éxito, se devuelve una respuesta JSON con un mensaje de éxito
+        # y un código de estado HTTP 201 (Creado).
+        return jsonify({"mensaje": "Producto agregado correctamente.", "codigo": nuevo_id}), 201
     else:
-        #Si el producto no se puede agregar, se devuelve una respuesta JSON con un mensaje de error y un código de estado HTTP 500 (Internal Server Error).
+        # Si el producto no se puede agregar, se devuelve una respuesta JSON con un mensaje de error
+        # y un código de estado HTTP 500 (Internal Server Error).
+        print ('316')
         return jsonify({"mensaje": "Error al agregar el producto."}), 500
 
 
 #--------------------------------------------------------------------
-# Modificar un producto según su código
+# Modificar uno, según su id
 #--------------------------------------------------------------------
 @app.route("/productos/<int:codigo>", methods=["PUT"])
-#La ruta Flask /productos/<int:codigo> con el método HTTP PUT está diseñada para actualizar la información de un producto existente en la base de datos, identificado por su código.
-#La función modificar_producto se asocia con esta URL y es invocada cuando se realiza una solicitud PUT a /productos/ seguido de un número (el código del producto).
+# La función modificar_producto se asocia con esta URL y es invocada cuando se realiza una solicitud PUT a /productos/ seguido de un número (el código del producto).
 def modificar_producto(codigo):
     #Se recuperan los nuevos datos del formulario
     nueva_descripcion = request.form.get("descripcion")
@@ -309,8 +372,7 @@ def modificar_producto(codigo):
 # Eliminar un producto según su código
 #--------------------------------------------------------------------
 @app.route("/productos/<int:codigo>", methods=["DELETE"])
-#La ruta Flask /productos/<int:codigo> con el método HTTP DELETE está diseñada para eliminar un producto específico de la base de datos, utilizando su código como identificador.
-#La función eliminar_producto se asocia con esta URL y es llamada cuando se realiza una solicitud DELETE a /productos/ seguido de un número (el código del producto).
+# La función eliminar_producto se asocia con esta URL y es llamada cuando se realiza una solicitud DELETE a /productos/ seguido de un número (el código del producto).
 def eliminar_producto(codigo):
     # Busco el producto en la base de datos
     producto = catalogo.consultar_producto(codigo)
